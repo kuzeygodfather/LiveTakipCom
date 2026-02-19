78330a76212887ee1e74027f10423c11ea06892e
kuzeygodfather <kuzeygodfather@gmail.com>
2026-02-20 02:07:54 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -189,7 +189,7 @@ function App() {
           </nav>
         </aside>
 
-        <main className="flex-1 lg:ml-64 pt-16 lg:pt-0 p-4 sm:p-6 lg:p-8 min-w-0">
+        <main className="flex-1 lg:ml-64 pt-16 lg:pt-8 p-4 sm:p-6 lg:p-8 min-w-0">
           {renderPage()}
         </main>
       </div>
